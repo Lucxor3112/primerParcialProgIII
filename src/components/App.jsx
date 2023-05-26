c85ab8f98b7edbecd9e2e1380328ee58b9f15535
Lucas <lucascaliva3112@gmail.com>
2023-05-25 21:36:56 -0300

    Caliva: agregue toda mi parte del proyecto

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import '../styles/App.css'
+import '../styles/Media.css'
+import Cards from './Cards.jsx'
+import Footer from './Footer'
+
+function App() {
+  return (
+    <>
+      <Cards></Cards>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+import { faUtensils } from '@fortawesome/free-solid-svg-icons'
+
+function Footer() {
+  return (
+    <>
+        <footer>
+            <div>
+                <div className='redes'>
+                <a className='face' href=""> 
+                    <FontAwesomeIcon icon={faFacebook} />
+                </a>
+                <a href="">  
+                    <FontAwesomeIcon icon={faInstagram} />
+                </a>
+                <a href="">
+                    <FontAwesomeIcon icon={faWhatsapp} />
+                </a>
+                
+                </div>
+                <h3 className='logo'>Antojos <FontAwesomeIcon icon={faUtensils} /></h3>
+                <p className='copyright'>Copyright© 2022 Antojos, Inc</p>
+                <div className='detalle'>
+                    <a href="#">Informacion legal</a>
+                    <a href="#">Politica de privacidad</a>
+                </div>
+            </div>
+        </footer>
+    </>
+  )
+}
+
+export default Footer
