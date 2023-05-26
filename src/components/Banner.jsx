bd10dafcd715b0d08840fc8205e0520d4c48dbea
facundolinares <132092183+facundolinares@users.noreply.github.com>
2023-05-25 22:20:03 -0300

    Create Banner.jsx
    Agregué el banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,18 @@
+function Main() {
+    return (
+      <>
+          <div className="banner">
+            <div className="capa">
+
+            </div>
+            <div className="info">
+                <h1>En Antojos, sos nuestra prioridad. Deléitate con la mejor comida y disfruta de un servicio excepcional ¡Te esperamos con gusto y pasión!</h1>
+                
+                   <a href="#"> Revisá nuestros platos</a>                  
+            </div>
+          </div>
+      </>
+    )
+  }
+  
+  export default Main
