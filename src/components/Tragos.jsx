ae6fa6f3a97334ee1369fa3ace8c0535ee25ec5a
Lucas <lucascaliva3112@gmail.com>
2023-07-03 14:14:57 -0300

    listo

--- src/components/Tragos.jsx
+++ src/components/Tragos.jsx
@@ -33,20 +33,18 @@ function Tragos(){
             <h2 id='Tragos' className='subtitulo tragosSubtitulo'>Tragos</h2>
             <div className='containerGeneral'>
                 <div className= "tragosContainer">
-                    {data.map((cocktail) => (
-                        <div className="containerImagen" key={cocktail.idDrink}>
-                            <img src={cocktail.strDrinkThumb} alt="#"/>
-                        </div>
-                    ))}
-                    
-                    {data.map((cocktail) => (
-                        <div className='containerInfo' key={cocktail.idDrink}>
-                            <h2 className= "tragosNombre">{cocktail.strDrink}</h2>
-                            <p className= "tragosInfo2"> <span>¡Descubre nuestra variedad de tragos artesanales y cócteles clásicos!</span> Desde los clásicos atemporales hasta nuestras propias creaciones de autor, cada trago es una obra maestra de la mixología. <span>También ofrecemos opciones sin alcohol</span> y una selección de cervezas, vinos y licores para maridar con tu comida. <span>¡Ven y disfruta de una experiencia única en nuestro acogedor y elegante entorno!</span></p>
-                            {/* <button onClick={fetchCocktailHandler}>Siguiente trago</button> */}
-                            <input className='tragosBoton' onClick={fetchCocktailHandler} type="submit" value="Siguiente trago" />
-                        </div>
-                    ))}     
+                        {data.map((cocktail) => (
+                            <div className="containerImagen" key={cocktail.idDrink}>
+                                <img src={cocktail.strDrinkThumb} alt="#"/>
+                            </div>
+                        ))}
+                        {data.map((cocktail) => (
+                            <div className='containerInfo' key={cocktail.idDrink}>
+                                <h2 className= "tragosNombre">{cocktail.strDrink}</h2>
+                                <p className= "tragosInfo2"> <span>¡Descubre nuestra variedad de tragos artesanales y cócteles clásicos!</span> Desde los clásicos atemporales hasta nuestras propias creaciones de autor, cada trago es una obra maestra de la mixología. <span>También ofrecemos opciones sin alcohol</span> y una selección de cervezas, vinos y licores para maridar con tu comida. <span>¡Ven y disfruta de una experiencia única en nuestro acogedor y elegante entorno!</span></p>
+                                <input className='tragosBoton' onClick={fetchCocktailHandler} type="submit" value="Siguiente trago" />
+                            </div>
+                        ))}  
                 </div>
             </div>
         </> 
