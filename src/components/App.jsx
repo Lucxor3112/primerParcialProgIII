903cc776fd370305a8d7c2aa10fc0d0cee8fbaa5
Lucas <lucascaliva3112@gmail.com>
2023-07-03 00:17:45 -0300

    Agregie la api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,14 @@ import '../styles/Form.css'
 import '../styles/Header.css'
 import '../styles/Ofertas.css'
 import '../styles/Media.css'
+import '../styles/Tragos.css'
 import Cards from './Cards.jsx'
 import Footer from './Footer'
 import Banner from './Banner.jsx'
 import Header from './Header'
 import Ofertas from './Ofertas'
 import Form from './Form'
+import Tragos from './Tragos'
 
 function App() {
   return (
@@ -20,6 +22,7 @@ function App() {
       <Banner></Banner>
       <Cards></Cards>
       <Ofertas></Ofertas>
+      <Tragos></Tragos>
       <Form></Form>
       <Footer></Footer>
      </>
---
+++ src/components/Tragos.jsx
@@ -0,0 +1,56 @@
+import {useState, useCallback, useEffect} from 'react';
+import axios from 'axios';
+
+function Tragos(){
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState([]);
+
+    const url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+
+    const fetchCocktailHandler = useCallback(()=> {
+     setLoading(true);
+
+     axios
+     .get(url)
+     .then(res=> {
+        console.log(res.data);
+        setData(res.data.drinks);
+     })
+     .catch(e => console.log(e))
+     .finally(() => setLoading(false));
+    }, []);
+
+    useEffect(() => {
+       fetchCocktailHandler();
+    }, [fetchCocktailHandler]);
+
+    if(loading){
+        return <h2 className='cargando'>Cargando...</h2>;
+    }
+
+    return( 
+        <>
+            <h2 className='subtitulo tragosSubtitulo'>Tragos</h2>
+            <div className='containerGeneral'>
+                <div className= "tragosContainer">
+                    {data.map((cocktail) => (
+                        <div className="containerImagen" key={cocktail.idDrink}>
+                            <img src={cocktail.strDrinkThumb} alt="#"/>
+                        </div>
+                    ))}
+                    
+                    {data.map((cocktail) => (
+                        <div className='containerInfo' key={cocktail.idDrink}>
+                            <h2 className= "tragosNombre">{cocktail.strDrink}</h2>
+                            <p className= "tragosInfo2"> <span>¡Descubre nuestra variedad de tragos artesanales y cócteles clásicos!</span> Desde los clásicos atemporales hasta nuestras propias creaciones de autor, cada trago es una obra maestra de la mixología. <span>También ofrecemos opciones sin alcohol</span> y una selección de cervezas, vinos y licores para maridar con tu comida. <span>¡Ven y disfruta de una experiencia única en nuestro acogedor y elegante entorno!</span></p>
+                            {/* <button onClick={fetchCocktailHandler}>Siguiente trago</button> */}
+                            <input className='tragosBoton' onClick={fetchCocktailHandler} type="submit" value="Siguiente trago" />
+                        </div>
+                    ))}     
+                </div>
+            </div>
+        </> 
+    );
+}
+
+export default Tragos;
